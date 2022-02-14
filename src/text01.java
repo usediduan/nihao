82c2bca5748bb9b5760c019ca889fb6b3e870199
admin <2876065879@qq.con>
2022-02-14 16:06:09 +0800

    111

--- src/text01.java
+++ src/text01.java
@@ -7,7 +7,7 @@ public class text01 {
         System.out.println("你在狗叫什么");
         System.out.println("我们是好朋友吗");
         System.out.println("你是不是汪汪队的小队长");
-        System.out.println("和号");
+        System.out.println("和号1111");
     }
 
 
