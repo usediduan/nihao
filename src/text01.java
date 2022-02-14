cfe314a6be5e177d952711d2ccebda467d94c58d
admin <2876065879@qq.con>
2022-02-14 15:26:57 +0800

    master01

--- src/text01.java
+++ src/text01.java
@@ -5,6 +5,7 @@ public class text01 {
     public static void main(String[] args) {
         System.out.println("你在叫什么");
         System.out.println("你在狗叫什么");
+        System.out.println("我们是好朋友吗");
     }
 
 
